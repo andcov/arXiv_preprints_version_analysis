533d0742d39ef7e6b4972c30520cc5e38ed84a4c
andcov <andreicovaci123@gmail.com>
2022-10-11 15:50:54 +0000

    Finish analysing cs and math papers

--- download.py
+++ download.py
@@ -3,31 +3,31 @@ import time
 import sys
 import pandas as pd
 
-mode = "math" # or "cs"
-download_path = "./papers"
+csv_path = "phys_papers.csv"
+download_path = "/mnt/volume_nyc1_01/phys/"
+downloaded_csv_path= "../downloaded_phys_papers.csv"
 
-if mode == "math":
-    csv_name = "math_papers.csv"
-    folder_name = download_path + "/math/"
-elif mode == "cs":
-    csv_name = "cs_papers.csv"
-    folder_name = download_path + "/cs/"
-else:
-    print("Available args are `math` or `cs`")
-    quit()
 
-papers = pd.read_csv(csv_name, parse_dates = ["date"], low_memory=False)
+papers = pd.read_csv(csv_path, parse_dates = ["date"], dtype = {'id': str}, low_memory=False)
 
-def download(x):
-    for i in range(1, x["versions"] + 1):
-        idx = x["id"].replace("/", "_")
+def download(paper):
+    for i in range(1, paper["total_versions"] + 1):
+        idx = paper["id"].replace("/", "_")
         name = idx + "v" + str(i) + ".pdf"
         url = "https://export.arxiv.org/pdf/" + name
         response = requests.get(url)
-        with open(folder_name + name, 'wb') as f:
+        with open(download_path + name, 'wb') as f:
             f.write(response.content)
-        print("Downloaded " + name)
-        time.sleep(1)
-    print()
+        time.sleep(0.2)
 
-papers.loc[papers["versions"] <= 4].sample(frac=1).head(50).apply(download, axis = 1)
+    downloaded_papers_id.append(paper["id"])
+    if len(downloaded_papers_id) % 10 == 0:
+        print("Downloaded: ", len(downloaded_papers_id))
+
+
+downloaded_papers_id = []
+to_download_papers = papers.drop_duplicates("id").sample(frac = 1).head(4000)
+to_download_papers.to_csv("to_download_phys.csv", index=False)
+to_download_papers.apply(download, axis = 1)
+
+papers.query("id in @downloaded_papers_id").reset_index(drop=True).to_csv(save_csv_path, index=False)
